9cd609b6ad8a44609ccabba8727da356a61a048d
obemeuche <obemeuchechi@gmail.com>
2024-07-26 02:07:30 +0100

    feat/2D covering and number of integers

---
+++ src/main/java/com/algorithm/leetcode75/Array2DArray.java
@@ -0,0 +1,37 @@
+package com.algorithm.leetcode75;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Array2DArray {
+
+    static int[][] fill2DArray()
+    {
+        Random rand = new Random();
+
+        int [][] arr = new int[3][3];
+        for(int row = 0; row < arr.length; row++){
+            for(int col = 0; col < arr[row].length; col++){
+                arr[row][col] = rand.nextInt(50) + 1;
+            }
+        }
+        System.out.println((Arrays.deepToString(arr)));
+        return arr;
+    }
+
+    static int [] findTarget (int [][] arr, int target){
+        for(int row = 0; row < arr.length; row++){
+            for(int col = 0; col < arr[row].length; col++){
+                if(arr[row][col] == target){
+                    return new int []{row,col};
+                }
+            }
+        }
+        return new int []{-1,-1};
+    }
+
+    public static void main(String[] args) {
+        int [][] arr = fill2DArray();
+        System.out.println(Arrays.toString(findTarget(arr, 50)));
+    }
+}
