f29bd73ade1894a25dcc991e7f0a5934d8d8e4ab
obemeuche <obemeuchechi@gmail.com>
2024-08-03 23:39:50 +0100

    feat/optimized solution for D17

--- src/main/java/com/algorithm/leetcode75/D17MaxConsecutiveOnes.java
+++ src/main/java/com/algorithm/leetcode75/D17MaxConsecutiveOnes.java
@@ -30,28 +30,24 @@ public class D17MaxConsecutiveOnes {
 
         int tracker = 0;
         for (int i = 0; i < nums.length; i++) {
-            int currentVal = nums[i];
+            if (nums[i] == 0) {
+                 tracker++;
+//                 count++;
+            }
             for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] == currentVal) {
+                if (nums[j] == 1)
+                {
                     count++;
                 }
-                if (tracker <= k){
-                    if (nums[j] == 0) {
-                        tracker++;
-                        count++;
-                    }else{
-                        tracker++;
-//                        continue;
+                if (nums[j] == 0 && tracker <= k)
+                {
+                    tracker++;
+                    count++;
+                    if(tracker > k){
+                        tracker = 0;
+                        count = 0;
+                        break;
                     }
-                }
-//                if (nums[j] == 0 && tracker <= k) {
-//                    tracker++;
-//                    count++;
-//                }
-                if (tracker > k) {
-                    tracker = 0;
-                    count = 0;
-                    break;
                 }
             }
             maxOneCount = Math.max(maxOneCount, count);
@@ -59,9 +55,25 @@ public class D17MaxConsecutiveOnes {
         return maxOneCount;
     }
 
+    static int longestOnes_Optimized(int[] nums, int k) {
+        //this approach uses sliding window
+        int i = 0;
+        int j = 0;
+        while (i < nums.length) {
+            if (nums[i] == 0) k--;
+            if (k < 0){
+                if (nums[j] == 0) k++;
+                j++;
+            }
+            i++;
+        }
+
+        return i - j;
+    }
+
     public static void main(String[] args) {
         int[] nums = {1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0};
         int k = 2;
-        System.out.println(longestOnes(nums, k));
+        System.out.println(longestOnes_Optimized(nums, k));
     }
 }
