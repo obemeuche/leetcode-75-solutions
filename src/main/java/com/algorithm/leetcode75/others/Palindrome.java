0fd32f0fb8966f8f8896f346334d9bc44863fcb0
obemeuche <obemeuchechi@gmail.com>
2024-08-05 16:55:30 +0100

    feat/loop 2ru half the string length

--- src/main/java/com/algorithm/leetcode75/others/Palindrome.java
+++ src/main/java/com/algorithm/leetcode75/others/Palindrome.java
@@ -2,20 +2,32 @@ package com.algorithm.leetcode75.others;
 
 public class Palindrome {
     public static void main(String[] args) {
-        String s = "abcdcba";
-        System.out.println(isPalindrome(s));
+        String s = "abcqba";
+        System.out.println(isPalindrome2(s));
     }
 
     static boolean isPalindrome(String s) {
         //check for null
         if (s == null || s.isEmpty()) return false;
 
+        //using 2 pointers initialized outside the loop
         int start = 0, end = s.length() - 1;
         while (start < end) {
             if (s.charAt(start) != s.charAt(end)) return false;
             start++;
             end--;
         }
+        return true;
+    }
+
+    static boolean isPalindrome2(String s) {
+        if (s == null || s.isEmpty()) return false;
+        //using pointers inside the while loop and looping through half the length of the string
+        for (int i = 0; i < s.length() / 2; i++) {
+            int end = s.length() - 1-i;
+            if (s.charAt(i) != s.charAt(end)) return false;
+        }
+
         return true;
     }
 }
