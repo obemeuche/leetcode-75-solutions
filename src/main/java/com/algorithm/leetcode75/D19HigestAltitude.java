931be0a28e11fc93af580f1abc3f7b6529df94b7
obemeuche <obemeuchechi@gmail.com>
2024-08-11 00:05:50 +0100

    feat/initiated D20

--- src/main/java/com/algorithm/leetcode75/D19HigestAltitude.java
+++ src/main/java/com/algorithm/leetcode75/D19HigestAltitude.java
@@ -2,6 +2,8 @@ package com.algorithm.leetcode75;
 
 public class D19HigestAltitude {
     /*
+        https://leetcode.com/problems/find-the-highest-altitude/description/?envType=study-plan-v2&envId=leetcode-75
+
         There is a biker going on a road trip. The road trip consists of n + 1 points at
         different altitudes. The biker starts his trip on point 0 with altitude equal 0.
         You are given an integer array gain of length n where gain[i] is the net gain in
