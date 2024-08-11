32d7c4e1516d4f784623d559ca49c6bbb78281c1
obemeuche <obemeuchechi@gmail.com>
2024-08-11 18:43:11 +0100

    feat/completed D21

--- src/main/java/com/algorithm/leetcode75/D21DifferenceOfTwoArrays.java
+++ src/main/java/com/algorithm/leetcode75/D21DifferenceOfTwoArrays.java
@@ -7,6 +7,8 @@ import java.util.Set;
 
 public class D21DifferenceOfTwoArrays {
     /*
+        https://leetcode.com/problems/find-the-difference-of-two-arrays/submissions/1352318322/?envType=study-plan-v2&envId=leetcode-75
+
         Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
         answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
         answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
