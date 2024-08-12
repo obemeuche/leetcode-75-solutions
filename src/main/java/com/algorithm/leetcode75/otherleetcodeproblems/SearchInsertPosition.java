70b0692e16d055d721a03354ffdeeebaea6960ba
obemeuche <obemeuchechi@gmail.com>
2024-08-12 14:56:19 +0100

    feat/completed search insert position using binary search

--- src/main/java/com/algorithm/leetcode75/otherleetcodeproblems/SearchInsertPosition.java
+++ src/main/java/com/algorithm/leetcode75/otherleetcodeproblems/SearchInsertPosition.java
@@ -22,8 +22,19 @@ public class SearchInsertPosition {
      */
 
     public static void main(String[] args) {
-
+        int [] arr = {1,3,5,6}; int target = 2;
+        System.out.println(searchInsert(arr, target));
     }
 
-
+    static int searchInsert(int[] nums, int target) {
+        //applying binary search and instead of returning -1, return the start index after the loop breaks
+        int start = 0; int end = nums.length - 1;
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if(nums[mid] == target) return mid;
+            else if(target > nums[mid]) start = mid + 1;
+            else end = mid - 1;
+        }
+        return start;
+    }
 }
