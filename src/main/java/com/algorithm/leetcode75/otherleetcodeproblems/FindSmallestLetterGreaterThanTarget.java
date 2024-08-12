c5057cb5da724f80c14d07255d3c3d257d5ce56a
obemeuche <obemeuchechi@gmail.com>
2024-08-12 16:47:41 +0100

    feat/completed smallest letter greater than target

--- src/main/java/com/algorithm/leetcode75/otherleetcodeproblems/FindSmallestLetterGreaterThanTarget.java
+++ src/main/java/com/algorithm/leetcode75/otherleetcodeproblems/FindSmallestLetterGreaterThanTarget.java
@@ -27,6 +27,19 @@ public class FindSmallestLetterGreaterThanTarget {
      */
 
     public static void main(String[] args) {
+        char [] letters = {'c','f','j'}; char target = 'c';
+        System.out.println(nextGreatestLetter(letters, target));
+    }
 
+    static char nextGreatestLetter(char[] letters, char target) {
+        int start = 0; int end = letters.length - 1;
+        while(start <= end){
+            int mid = (start + end)/2;
+            if(target >= letters[mid]) start = mid + 1;
+            else end = mid - 1;
+        }
+        if(start == letters.length) return letters[0];
+        return letters[start];
+        // return letters[start % letters.length];
     }
 }
