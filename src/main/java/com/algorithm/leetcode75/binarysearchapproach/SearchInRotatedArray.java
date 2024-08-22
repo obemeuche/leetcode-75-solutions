09cc3b4e1316f73c90dfb89412d6b31fe343e760
obemeuche <obemeuchechi@gmail.com>
2024-08-22 18:12:36 +0100

    feat/implemented search in rotated array

--- src/main/java/com/algorithm/leetcode75/binarysearchapproach/SearchInRotatedArray.java
+++ src/main/java/com/algorithm/leetcode75/binarysearchapproach/SearchInRotatedArray.java
@@ -26,6 +26,65 @@ public class SearchInRotatedArray {
      */
 
     public static void main(String[] args) {
+        int[] arr = {4,5,6,7,0,1,2}; int target = 0;
 
+        //[1,3] target = 3;
+        System.out.println(searchInRotatedArray(arr,target));
+    }
+
+    static int searchInRotatedArray(int[] nums, int target) {
+        int peakIndex = findPeakElement(nums);
+        //check from left position
+        boolean isAsc = true;
+        int leftIndex = binarySearch(nums, target, 0, peakIndex, false);
+        if (leftIndex != -1){
+            return leftIndex;
+        }
+        //check from the right position
+        return binarySearch(nums, target, peakIndex + 1, nums.length - 1, true);
+    }
+
+    static int findPeakElement(int[] arr) {
+        int start = 0; int end = arr.length - 1;
+
+        //ans will be when start = end
+        while(start != end){
+            int mid = (start+end)/2;
+            if(arr[mid] > arr[mid+1]){
+                //you are in the decreasing part of the array
+                //look left
+                end = mid;
+            }else {
+                //you are in the increasing part of the array
+                //look right
+                start = mid+1;
+            }
+        }
+        //start will be equal to end and pointing to the largest number
+        //start and end are always trying to find the max element in the above checks
+        return start; //or end as both are equal
+    }
+
+    static int binarySearch(int[] arr, int target, int start, int end, boolean isAsc) {
+        while(start <= end){
+            int mid = (start+end)/2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            if(isAsc){
+                if(target > arr[mid]){
+                    start = mid+1;
+                }else{
+                    end = mid-1;
+                }
+            }else{
+                if(target < arr[mid]){
+                    start = mid+1;
+                }else{
+                    end = mid-1;
+                }
+            }
+        }
+        return -1;
     }
 }
