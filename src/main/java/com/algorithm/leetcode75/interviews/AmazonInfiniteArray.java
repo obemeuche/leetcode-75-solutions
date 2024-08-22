c0777ab24afaad4dde92c000561416e1108f926e
obemeuche <obemeuchechi@gmail.com>
2024-08-22 13:33:14 +0100

    feat/implement binary search on an infinite array

---
+++ src/main/java/com/algorithm/leetcode75/interviews/AmazonInfiniteArray.java
@@ -0,0 +1,42 @@
+package com.algorithm.leetcode75.interviews;
+
+public class AmazonInfiniteArray {
+    // https://www.geeksforgeeks.org/find-position-element-sorted-array-infinite-numbers/
+    // Find position of an element in a sorted array of infinite numbers
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5,6,7,8,9}; int target = 5;
+        System.out.println(infiniteArray(arr, target));
+    }
+
+    static int infiniteArray(int[] arr, int target) {
+        //since array is infinite, we can't get the length using arr.length
+        //we take the end in bits and then double the length
+        int start = 0;
+        int end = 1;
+
+        while (target > arr[end]) {
+            //temp to hold new start position
+            int temp = end + 1;
+            //double the box between start and end position
+            end = end + (end - start + 1) * 2;
+            start = temp;
+        }
+
+        return binarySearch(arr, target, start, end);
+    }
+
+    static int binarySearch(int[] arr, int target, int start, int end) {
+
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if(arr[mid] == target) {
+                return mid;
+            }else if(target > arr[mid]) {
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
