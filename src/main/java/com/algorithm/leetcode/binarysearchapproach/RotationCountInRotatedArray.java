31f1aba7c9058c81496f4a530a3f98c865d45877
obemeuche <obemeuchechi@gmail.com>
2024-08-27 16:53:20 +0100

    feat/implemented count in a rotated array

--- src/main/java/com/algorithm/leetcode/binarysearchapproach/RotationCountInRotatedArray.java
+++ src/main/java/com/algorithm/leetcode/binarysearchapproach/RotationCountInRotatedArray.java
@@ -18,14 +18,21 @@ public class RotationCountInRotatedArray {
         //validate array is not empty
         if (arr == null || arr.length == 0) return 0;
         //find the index of the peakElement in the rotated array
+        int peak = findPivot(arr);
+        //and return the count of the peak element in the array
+        return peak + 1;
+
+    }
+
+    private static int findPivot(int [] arr){
         int start = 0; int end = arr.length - 1;
         while (start != end){
             int mid = (start + end) / 2;
             if (mid < end && arr[mid] > arr[mid + 1]){
-                return mid + 1;
+                return mid;
             }
             if (mid > start && arr[mid] < arr[mid - 1]){
-                return mid;
+                return mid - 1;
             }
             if (arr[mid] >= arr[start]){
                 start = mid + 1;
@@ -33,6 +40,6 @@ public class RotationCountInRotatedArray {
                 end = mid - 1;
             }
         }
-        return 0;
+        return -1;
     }
 }
