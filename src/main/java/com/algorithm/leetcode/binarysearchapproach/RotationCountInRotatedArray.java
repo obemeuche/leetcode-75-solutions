e1e6200afb584ef766b3dde43b3bb9ed7a893f71
obemeuche <obemeuchechi@gmail.com>
2024-08-27 16:33:25 +0100

    feat/implemented count in a rotated array

---
+++ src/main/java/com/algorithm/leetcode/binarysearchapproach/RotationCountInRotatedArray.java
@@ -0,0 +1,38 @@
+package com.algorithm.leetcode.binarysearchapproach;
+
+public class RotationCountInRotatedArray {
+    /*
+        Given an array arr[] of size N having distinct numbers sorted in increasing order and
+        the array has been right rotated (i.e, the last element will be cyclically shifted to the
+        starting position of the array) k number of times, the task is to find the value of k.
+     */
+
+    public static void main(String[] args) {
+//        int[] arr = {15, 18, 2, 3, 6, 12};
+//        int[] arr = {7, 9, 11, 12, 5};
+        int[] arr = {7, 9, 11, 12, 15};
+        System.out.println(rotationCount(arr));
+    }
+
+    private static int rotationCount(int [] arr){
+        //validate array is not empty
+        if (arr == null || arr.length == 0) return 0;
+        //find the index of the peakElement in the rotated array
+        int start = 0; int end = arr.length - 1;
+        while (start != end){
+            int mid = (start + end) / 2;
+            if (mid < end && arr[mid] > arr[mid + 1]){
+                return mid + 1;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]){
+                return mid;
+            }
+            if (arr[mid] >= arr[start]){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        return 0;
+    }
+}
