aa71707d93f6be17a14de988eec13e99ebcb2686
obemeuche <obemeuchechi@gmail.com>
2024-08-29 18:33:31 +0100

    feat/initiated class

---
+++ src/main/java/com/algorithm/leetcode/binarysearchapproach/IsArraySortedAndRotated.java
@@ -0,0 +1,27 @@
+package com.algorithm.leetcode.binarysearchapproach;
+
+public class IsArraySortedAndRotated {
+    /*
+        https://www.geeksforgeeks.org/check-if-an-array-is-sorted-and-rotated/
+
+        Given an array arr[] of size n, the task is to return true if it was originally
+        sorted in non-decreasing order and then rotated (including zero rotations).
+        Otherwise, return false. The array may contain duplicates.
+
+        Examples:
+        Input: arr[] = { 3, 4, 5, 1, 2 }
+        Output: YES
+        Explanation: The above array is sorted and rotated
+        Sorted array: {1, 2, 3, 4, 5}
+        Rotating this sorted array clockwise
+        by 3 positions, we get: { 3, 4, 5, 1, 2}
+
+        Input: arr[] = {3, 4, 6, 1, 2, 5}
+        Output: NO
+        Explanation: The above array can’t be rotated clockwise to form the sorted array.
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
