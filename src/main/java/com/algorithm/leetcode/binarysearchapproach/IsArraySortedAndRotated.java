99811db62e5f1428fc974c7ca34018f7879a30f1
obemeuche <obemeuchechi@gmail.com>
2024-09-08 00:57:34 +0100

    feat/checking array rotation

--- src/main/java/com/algorithm/leetcode/binarysearchapproach/IsArraySortedAndRotated.java
+++ src/main/java/com/algorithm/leetcode/binarysearchapproach/IsArraySortedAndRotated.java
@@ -22,6 +22,36 @@ public class IsArraySortedAndRotated {
      */
 
     public static void main(String[] args) {
+//        int [] arr = {3, 4, 5, 1, 2};
+        int [] arr = {3, 4, 6, 1, 2, 5};
+        System.out.println(isRotatedAndSorted(arr));
+    }
 
+    private static boolean isRotatedAndSorted(int[] arr) {
+        int n = findPivot(arr);
+        if(n == - 1) return false;
+        return true;
+//        return findPivot(arr) + 1 == 0;
+    }
+
+    private static int findPivot(int[] arr) {
+        int start = 0, end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (end > mid && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+            if (arr[mid] >= arr[start]) {
+                start = mid + 1;
+            }else {
+                end = mid - 1;
+            }
+        }
+        //return count == 1 && arr[arr.length - 1] < arr[0];
+        return -1;
     }
 }
