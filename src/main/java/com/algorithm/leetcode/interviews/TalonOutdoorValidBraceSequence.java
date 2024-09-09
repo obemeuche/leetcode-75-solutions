ad7eb0ed5142d6fe7f0123cbe184ed1975fe6621
obemeuche <obemeuchechi@gmail.com>
2024-09-09 23:53:01 +0100

    feat/initialized valid brace sequence

---
+++ src/main/java/com/algorithm/leetcode/interviews/TalonOutdoorValidBraceSequence.java
@@ -0,0 +1,17 @@
+package com.algorithm.leetcode.interviews;
+
+public class TalonOutdoorValidBraceSequence {
+    /*
+        You are given a list of strings, where each string contains only the characters (), {}, and [].
+        Write a function that determines if each string is a valid sequence of braces.
+        A string is considered valid if:
+        Every opening brace ((, {, [) has a corresponding closing brace (), }, ]).
+        Braces must close in the correct order. For example, "{[()]}" is valid but "{[}]" is not.
+        Your task is to implement a function that returns a list of strings, where each entry is
+        either "YES" (if the corresponding input string is valid) or "NO" (if it is not).
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
