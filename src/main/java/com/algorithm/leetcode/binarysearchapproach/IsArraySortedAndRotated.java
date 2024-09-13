bd602550b593c56cbb26ad725ae9eae4500663be
obemeuche <obemeuchechi@gmail.com>
2024-09-13 23:58:16 +0100

    feat/completed a second method for longest parenthesis

--- src/main/java/com/algorithm/leetcode/binarysearchapproach/IsArraySortedAndRotated.java
+++ src/main/java/com/algorithm/leetcode/binarysearchapproach/IsArraySortedAndRotated.java
@@ -25,39 +25,39 @@ public class IsArraySortedAndRotated {
         int [] arr = {1, 2, 3, 4, 5};
 //        int [] arr = {3, 4, 5, 1, 2};
 //        int [] arr = {3, 4, 6, 1, 2, 5};
-//        System.out.println(isRotatedAndSorted(arr));
-        System.out.println(check(arr));
+        System.out.println(isRotatedAndSorted(arr));
+//        System.out.println(check(arr));
     }
 
-//    private static boolean isRotatedAndSorted(int[] arr) {
-//        int n = findPivot(arr);
-//        if(n == - 1) return false;
-//        return true;
-////        return findPivot(arr) + 1 == 0;
-//    }
-//
-//    private static int findPivot(int[] arr) {
-//        int start = 0, end = arr.length - 1;
-//
-//        while (start <= end) {
-//            int mid = (start + end) / 2;
-//            if (end > mid && arr[mid] > arr[mid + 1]) {
-//                return mid;
-//            }
-//            if (mid > start && arr[mid] < arr[mid - 1]) {
-//                return mid - 1;
-//            }
-//            if (arr[mid] >= arr[start]) {
-//                start = mid + 1;
-//            }else {
-//                end = mid - 1;
-//            }
-//        }
-//        //return count == 1 && arr[arr.length - 1] < arr[0];
-//        return -1;
-//    }
+    private static boolean isRotatedAndSorted(int[] arr) {
+        int n = findPivot(arr);
+        if(n == - 1) return false;
+        return true;
+//        return findPivot(arr) + 1 == 0;
+    }
 
-    public static boolean check(int[] arr)
+    private static int findPivot(int[] arr) {
+        int start = 0, end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (end > mid && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+            if (arr[mid] >= arr[start]) {
+                start = mid + 1;
+            }else {
+                end = mid - 1;
+            }
+        }
+        //return count == 1 && arr[arr.length - 1] < arr[0];
+        return -1;
+    }
+
+    public static boolean isArraySortedAndRotated(int[] arr)
     {
         int n = arr.length;
         // Initialize count of the number of times the
