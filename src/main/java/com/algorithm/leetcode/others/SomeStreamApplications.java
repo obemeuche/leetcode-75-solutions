545ee885a90410daf4237c29afd25744bda7430e
obemeuche <obemeuchechi@gmail.com>
2024-09-16 19:18:24 +0100

    feat/using streams

--- src/main/java/com/algorithm/leetcode/others/SomeStreamApplications.java
+++ src/main/java/com/algorithm/leetcode/others/SomeStreamApplications.java
@@ -8,6 +8,7 @@ public class SomeStreamApplications {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         System.out.println(sumOfEvenNumbers(list));
+        System.out.println(countString());
     }
 
     // 1. Find the sum of all even numbers in a list of integers.
@@ -20,6 +21,13 @@ public class SomeStreamApplications {
         return sum;
     }
 
+    //  Find and print the count of strings that have length greater than 5.
+    private static int countString(){
+        List<String> list = Arrays.asList("apple", "banana", "carrot", "dove", "ewi", "farm", "guava", "hurt");
 
+        return (int) list.stream().
+                filter(str -> str.length() >= 5).
+                count();
+    }
 
 }
