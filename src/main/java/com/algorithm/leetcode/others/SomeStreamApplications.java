9633b377b2a0d9d3bbcc12822f6b593c75a9649e
obemeuche <obemeuchechi@gmail.com>
2024-09-16 23:05:35 +0100

    feat/using streams for operations

--- src/main/java/com/algorithm/leetcode/others/SomeStreamApplications.java
+++ src/main/java/com/algorithm/leetcode/others/SomeStreamApplications.java
@@ -2,6 +2,7 @@ package com.algorithm.leetcode.others;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SomeStreamApplications {
 
@@ -9,6 +10,8 @@ public class SomeStreamApplications {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         System.out.println(sumOfEvenNumbers(list));
         System.out.println(countString());
+        System.out.println(squareOfIntegers(list));
+        System.out.println(maxElement(list));
     }
 
     // 1. Find the sum of all even numbers in a list of integers.
@@ -28,6 +31,20 @@ public class SomeStreamApplications {
         return (int) list.stream().
                 filter(str -> str.length() >= 5).
                 count();
+    }
+
+    // 3. Implement a function that takes a list of integers as input and returns a new list containing the square of each element.
+    private static List<Integer> squareOfIntegers(List<Integer> numbers) {
+        return numbers.stream()
+                .map(num -> num * num)
+                .collect(Collectors.toList());
+    }
+
+    // 4. Find the maximum element in a list of integers.
+    private static int maxElement(List<Integer> numbers) {
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .max().getAsInt();
     }
 
 }
